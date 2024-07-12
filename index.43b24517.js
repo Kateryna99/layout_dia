f6edd6c9a872071d863aa3254dc2b6a76c7be267
Katya <katya.stelmakhova@gmail.com>
2024-07-12 23:15:42 +0200

    make build

---
+++ index.43b24517.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",slidesPerView:1,loop:!0,autoplay:{delay:2500},navigation:{nextEl:".slide-card__button--next",prevEl:".slide-card__button--prev"}});
+//# sourceMappingURL=index.43b24517.js.map
